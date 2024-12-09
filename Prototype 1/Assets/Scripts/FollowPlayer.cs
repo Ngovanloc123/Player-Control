eb7c7c7db3b14a23da111ac8c252b3ce92c975d3
NgoVanLoc <62205ngovanloc@gmail.com>
2024-12-10 01:48:35 +0700

    add Assets

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Làm cho camera không có hiện tượng jittering
+    void LateUpdate()
+    {
+        // Thêm vector để camera phía sao object
+        transform.position = player.transform.position + offset; 
+    }
+}
